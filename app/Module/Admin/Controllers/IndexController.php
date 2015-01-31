df8de2974c009f0ad95ae269e188cc3a20576e66
BAUDOIN Mickael <mickael.baudoin@gmail.com>
2015-01-31 16:17:56 +0100

    Update IndexController.php

--- app/Module/Admin/Controllers/IndexController.php
+++ app/Module/Admin/Controllers/IndexController.php
@@ -6,6 +6,6 @@ use Symfony\Component\HttpFoundation\Request;
 class IndexController extends \LibApp\Controllers\AbstractController{
 
 	public function index(Request $request, Application $app){
-		return $app['twig']->render( '@admin/index/index.html', array());
+		return $this->render( '@admin/index/index.html', array());
 	}
 }
